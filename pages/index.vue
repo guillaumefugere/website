aa789a318650ab5b7f52febba6c2f6320232eda9
Guillaume Fugère <fugereg@gmail.com>
2020-11-19 19:39:02 -0500

    Basic Nuxt install

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Guillaume Fugère</h1>
+        <h2>Frontend Developer</h2>
+        <nav>
+            <nuxt-link to="/mvp">Minimum viable product</nuxt-link>
+        </nav>
+    </div>
+</template>
---
+++ pages/mvp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Guillaume Fugère</h1>
+        <p>
+            This website is build with MVP in mind.<br>
+            My website is online, this is my MVP :)<br>
+            Things might seems barebone, but will improve over time.
+        </p>
+        <nav>
+            <nuxt-link to="/">Homepage</nuxt-link>
+        </nav>
+    </div>
+</template>
